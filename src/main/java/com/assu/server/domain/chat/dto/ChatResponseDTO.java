480415b01f000e28530934265e07d2dcd7219c23
BAEK0111 <baekjw111@gmail.com>
2025-07-26 19:10:33 +0900

    feat/#16-chatting - 채팅방 조회 Response

--- src/main/java/com/assu/server/domain/chat/dto/ChatResponseDTO.java
+++ src/main/java/com/assu/server/domain/chat/dto/ChatResponseDTO.java
@@ -1,4 +1,32 @@
 package com.assu.server.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class ChatResponseDTO {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ChatRoomListResultDTO {
+        private String roomId;
+        private String lastMessage;
+        private LocalDateTime lastMessageTime;
+        private int unreadMessagesCount;
+        private Opponent opponent;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Opponent {
+        private Long  opponentId;
+        private String opponentName;
+        private String profileImageUrl;
+    }
 }
