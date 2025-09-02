256bdd2c95403365d1e604d281b72a7eec28dd73
2ghrms <aa020228@gmail.com>
2025-09-03 03:05:19 +0900

    refactor/#51: 테스트 환경에서 redis 제외 및 Mock bean으로 등록

--- src/test/java/com/assu/server/ServerApplicationTests.java
+++ src/test/java/com/assu/server/ServerApplicationTests.java
@@ -7,6 +7,8 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -22,8 +24,18 @@ class ServerApplicationTests {
 		FirebaseMessaging firebaseMessaging() {
 			return Mockito.mock(FirebaseMessaging.class);
 		}
-	}
 
+		@Bean
+		RedisConnectionFactory redisConnectionFactory() {
+			return Mockito.mock(RedisConnectionFactory.class);
+		}
+
+		@Bean
+        @SuppressWarnings("unchecked")
+		RedisTemplate<String, Object> redisTemplate() {
+			return Mockito.mock(RedisTemplate.class);
+		}
+	}
 
 	@Test
 	void contextLoads() {
