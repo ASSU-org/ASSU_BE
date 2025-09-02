6b4e04059b0c6514d234e759455e4edd4e73f1b6
2ghrms <aa020228@gmail.com>
2025-09-03 01:37:36 +0900

    refactor/#51: firebase 테스트용 Mock bean 주입 및 firebase configuaration은 test이외의 환경에서 주입

--- src/test/java/com/assu/server/ServerApplicationTests.java
+++ src/test/java/com/assu/server/ServerApplicationTests.java
@@ -1,12 +1,29 @@
 package com.assu.server;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ServerApplicationTests {
+
+	@Mock
+	private FirebaseMessaging firebaseMessaging;
+
+	@TestConfiguration
+	static class MockConfig {
+		@Bean
+		FirebaseMessaging firebaseMessaging() {
+			return Mockito.mock(FirebaseMessaging.class);
+		}
+	}
+
 
 	@Test
 	void contextLoads() {
