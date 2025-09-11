540b058f0e87917a051b4d71d559660646289d6c
kimyw1018 <poppppp00@gmail.com>
2025-09-11 23:24:26 +0900

    [Merge/#13] 충돌 해결

--- src/main/java/com/assu/server/domain/mapping/controller/StudentAdminController.java
+++ src/main/java/com/assu/server/domain/mapping/controller/StudentAdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/dashBoard")
+@RequestMapping("/admin/dashBoard")
 public class StudentAdminController {
     private final StudentAdminService studentAdminService;
     @Operation(
