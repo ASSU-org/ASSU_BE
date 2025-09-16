092378c07af88344d61cc1a9b8032cbe7e8f7c72
BAEK0111 <baekjw111@gmail.com>
2025-09-16 23:15:36 +0900

    refactor/#38 - chatting을 위한 SecurityConfig, WebSocketConfig 수정

--- src/main/java/com/assu/server/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/assu/server/domain/chat/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.assu.server.domain.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -8,25 +9,27 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")  // 클라이언트 WebSocket 연결 지점
+        registry.addEndpoint("/ws")  // 클라이언트 WebSocket 연결 지점
                 .setAllowedOriginPatterns(
+                        "*",
+                        "https://assu.shop",
                         "http://localhost:63342",
                         "http://localhost:5173",     // Vite 기본
                         "http://localhost:3000",     // CRA/Next 기본
                         "http://127.0.0.1:*",
-                        "http://192.168.*.*:*")       // 같은 LAN의 실제 기기 테스트용
-                .withSockJS();             // fallback for old browsers
+                        "http://192.168.*.*:*");       // 같은 LAN의 실제 기기 테스트용
+                      // fallback for old browsers
 
-        // ✅ 모바일/안드로이드용 (네이티브 WebSocket)
-        registry.addEndpoint("/ws/chat-native")
-                .setAllowedOriginPatterns("*"); // wss 사용 시 TLS 세팅
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트가 보내는 prefix
         registry.enableSimpleBroker("/sub"); // 서버가 보내는 prefix
+        registry.enableSimpleBroker("/certification"); // 인증현황을 받아보기 위한 구독 주소
+        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 인증 요청을 보내는 주소
     }
 }
