5a5623fa14f4e7826c7b15840ffe39ed1c5e19a9
eeeeeaaan <wjdwjddla73@naver.com>
2025-09-22 17:18:05 +0900

    [FIX/#109] 실시간 인증 문제 해결

--- src/main/java/com/assu/server/domain/certification/config/CertifyWebSocketConfig.java
+++ src/main/java/com/assu/server/domain/certification/config/CertifyWebSocketConfig.java
@@ -11,27 +11,27 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 import lombok.RequiredArgsConstructor;
 
-@EnableWebSocketMessageBroker
-@Configuration
-@RequiredArgsConstructor
-public class CertifyWebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-	private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/certification"); // 인증현황을 받아보기 위한 구독 주소
-		config.setApplicationDestinationPrefixes("/app"); // 클라이언트가 인증 요청을 보내는 주소
-	}
-
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOriginPatterns("*");          // 클라이언트 WebSocket 연결 주소
-			// .setAllowedOriginPatterns("http://10.0.2.2:8080", "ws://10.0.2.2:8080");// CORS 허용
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(stompAuthChannelInterceptor);
-	}
-
-}
+// @EnableWebSocketMessageBroker
+// @Configuration
+// @RequiredArgsConstructor
+// public class CertifyWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+// 	private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
+// 	@Override
+// 	public void configureMessageBroker(MessageBrokerRegistry config) {
+// 		config.enableSimpleBroker("/certification"); // 인증현황을 받아보기 위한 구독 주소
+// 		config.setApplicationDestinationPrefixes("/app"); // 클라이언트가 인증 요청을 보내는 주소
+// 	}
+//
+// 	@Override
+// 	public void registerStompEndpoints(StompEndpointRegistry registry) {
+// 		registry.addEndpoint("/ws-certify").setAllowedOriginPatterns("*");          // 클라이언트 WebSocket 연결 주소
+// 			// .setAllowedOriginPatterns("http://10.0.2.2:8080", "ws://10.0.2.2:8080");// CORS 허용
+// 	}
+//
+// 	@Override
+// 	public void configureClientInboundChannel(ChannelRegistration registration) {
+// 		registration.interceptors(stompAuthChannelInterceptor);
+// 	}
+//
+// }
