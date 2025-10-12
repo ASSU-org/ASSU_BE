a72cf1c9b7985a9147cd9b2c62d7d4c9054ae964
eeeeeaaan <wjdwjddla73@naver.com>
2025-10-12 21:13:12 +0900

    [fix/#143] best store 조회 방법 수정

--- src/main/java/com/assu/server/domain/user/repository/PartnershipUsageRepository.java
+++ src/main/java/com/assu/server/domain/user/repository/PartnershipUsageRepository.java
@@ -14,14 +14,13 @@ import com.assu.server.domain.user.entity.PartnershipUsage;
 public interface PartnershipUsageRepository extends JpaRepository<PartnershipUsage, Long> {
 
 	@Query(value = """
-        SELECT place
-        FROM partnership_usage
-        WHERE date >= CONVERT_TZ(CURDATE(), '+00:00', '+09:00')
-          AND date <  CONVERT_TZ(CURDATE() + INTERVAL 1 DAY, '+00:00', '+09:00')
-        GROUP BY place
-        ORDER BY COUNT(*) DESC
-        LIMIT 10
-        """, nativeQuery = true)
+       SELECT place
+       FROM partnership_usage
+       WHERE date = DATE(DATE_ADD(NOW(), INTERVAL 9 HOUR))
+       GROUP BY place
+       ORDER BY COUNT(*) DESC
+       LIMIT 10
+       """, nativeQuery = true)
 	List<String> findTodayPopularPartnership();
 
 	@Query("SELECT pu FROM PartnershipUsage pu " +
