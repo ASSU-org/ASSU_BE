a913ea6aa076af591b83c5d5e907e58e5755dffb
BAEK0111 <baekjw111@gmail.com>
2025-10-25 15:42:06 +0900

    refactor/#38 - 채팅 코드 리팩토링

--- src/main/java/com/assu/server/domain/chat/service/ChatService.java
+++ src/main/java/com/assu/server/domain/chat/service/ChatService.java
@@ -3,12 +3,15 @@ package com.assu.server.domain.chat.service;
 import com.assu.server.domain.chat.dto.ChatRequestDTO;
 import com.assu.server.domain.chat.dto.ChatResponseDTO;
 import com.assu.server.domain.chat.dto.ChatRoomListResultDTO;
+import com.assu.server.domain.chat.dto.MessageHandlingResult;
+
 import java.util.List;
 
 public interface ChatService {
     List<ChatRoomListResultDTO> getChatRoomList(Long memberId);
     ChatResponseDTO.CreateChatRoomResponseDTO createChatRoom(ChatRequestDTO.CreateChatRoomRequestDTO request, Long memberId);
-    ChatResponseDTO.SendMessageResponseDTO handleMessage(ChatRequestDTO.ChatMessageRequestDTO request);
+//    ChatResponseDTO.SendMessageResponseDTO handleMessage(ChatRequestDTO.ChatMessageRequestDTO request);
+    MessageHandlingResult handleMessage(ChatRequestDTO.ChatMessageRequestDTO request);
     ChatResponseDTO.ReadMessageResponseDTO readMessage(Long roomId, Long memberId);
     ChatResponseDTO.ChatHistoryResponseDTO readHistory(Long roomId, Long memberId);
     ChatResponseDTO.LeaveChattingRoomResponseDTO leaveChattingRoom(Long roomId, Long memberId);
